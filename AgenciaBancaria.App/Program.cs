580c7f50ec37243f7ed86fb27c3d412f6c291958
Viniciusadm <viniciusam.22@gmail.com>
2021-09-20 16:21:51 -0300

    Criação do front end do projeto

--- AgenciaBancaria.App/Program.cs
+++ AgenciaBancaria.App/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using AgenciaBancaria.Dominio;
 
-namespace AgenciaBancaria.App
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace AgenciaBancaria.App {
+    class Program {
+        static void Main(string[] args){
+            try {
+                Endereco endereco = new Endereco("Endereço", "99999999", "Cidade", "Estado");
+                Cliente cliente = new Cliente("Vinícius", "99999999999", "9999999", endereco);
+                ContaCorrente contaCorrente = new ContaCorrente(cliente, 100);
+                Console.WriteLine(contaCorrente + "\n");
+                string senha = "@AbC1234";
+                contaCorrente.Abrir(senha);
+                contaCorrente.Depositar(50, senha);
+                contaCorrente.ConsultarSaldo(senha);
+                contaCorrente.Sacar(100, senha);
+                contaCorrente.ConsultarSaldo(senha);
+                contaCorrente.Depositar(200, senha);
+                contaCorrente.ConsultarSaldo(senha);
+                contaCorrente.Sacar(50, senha);
+                contaCorrente.ConsultarSaldo(senha);
+                contaCorrente.Sacar(100, senha);
+                contaCorrente.ConsultarSaldo(senha);
+                contaCorrente.Sacar(50, senha);
+                contaCorrente.ConsultarSaldo(senha);
+                contaCorrente.Depositar(100, senha);;
+                contaCorrente.ConsultarSaldo(senha);
+            } catch (Exception erro) {
+                Console.WriteLine(erro);
+            }
         }
     }
 }
